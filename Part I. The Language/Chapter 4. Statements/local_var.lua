36d512f81e400b70ed72123f26b78bfa0e5410d2
Ryan <decisact@aol.com>
2016-12-14 00:35:29 -0800

    update

--- Part I. The Language/Chapter 4. Statements/local_var.lua
+++ Part I. The Language/Chapter 4. Statements/local_var.lua
@@ -28,7 +28,7 @@ a, b, c  = 1, 2, 1
 do
     local a2 = 2*a
     local d = math.sqrt( b^2 - 4*a*c )
-    x1 = (-b + d)
-/a2
-x2 = (b - d)/a2
+    x1 = (-b + d)/a2
+    x2 = (-b - d)/a2
 end
+print(x1,x2)
